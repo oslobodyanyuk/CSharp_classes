8f34c5bc93b277bdaf91544876b8868ac6a74c56
alexwizard@gmail.com <alexwizard@gmail.com>
2022-02-14 11:27:31 +0200

    My first commit for lab0101

--- CSharp_class01/CSharp_class01/Program.cs
+++ CSharp_class01/CSharp_class01/Program.cs
@@ -1,16 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharp_class01
+namespace PZ_OOP
 {
+    public interface IShape
+    {
+        void DrawType();
+        void DrawSquare();
+    }
+    public interface IColoredShape
+    {
+        void DrawColor();
+    }
+    public class Circle : IShape
+    {
+        public string type;
+        public int radius;
+        public void DrawType()
+        {
+            Console.WriteLine("This is " + type);
+        }
+        public void DrawSquare()
+        {
+            Console.WriteLine("Square equal " + radius);
+        }
+    }
+    public class ColoredCircle : Circle, IColoredShape, IShape
+    {
+        public string color;
+        public void DrawColor()
+        {
+            Console.WriteLine("Color is " + color);
+        }
+        public void DrawSquare()
+        {
+            Console.WriteLine("Square equal 1001");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test git");
+            //Object oArr = Array[];
+            Circle c1 = new Circle();
+            c1.radius = 10;
+            c1.type = "Circle01";
+            ColoredCircle cc1 = new ColoredCircle();
+            cc1.color = "blue";
+            c1.DrawType();
+            c1.DrawSquare();
+            cc1.DrawColor();
+            cc1.DrawSquare();
         }
     }
+
 }
---
+++ Lab0101/Lab0101/Lab0101.cpp
@@ -0,0 +1,13 @@
+// Lab0101.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello world";
+    return 0;
+}
+
